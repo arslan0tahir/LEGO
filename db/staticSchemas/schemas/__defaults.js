a6ec3646a54c8a35f87c67dacb48a00c59b769b8
Arslan Tahir <arslan.tahir@outlook.com>
2020-01-11 15:26:21 +0500

    restructuring

--- db/staticSchemas/schemas/__defaults.js
+++ db/staticSchemas/schemas/__defaults.js
@@ -10,18 +10,22 @@
 //     rowPerm BIGINT,
 //     cellPerm JSON,
 
-let __defaults={
-    id:{
-        formDataType:['id'], //text,text box,id ,drop down,number, lookup, file, picture, cluster
+let __defaults=[
+    {
+        columnName: 'id', 
+        formDataType:'id', //text,text box,id ,drop down,number,datetime ,lookup, file, picture, cluster
         dbDataType: 'VARCHAR(255)',//can be selected automatically based upon form datatype
+            //auto
             //text its VARCHAR(0-65536)
             //textBox VARCHAR(0-65533)
             //number VARCHAR(0-255)
             //cluster JSON
-            //lookup BIGINT
+            //lookup BIGINT foriegn key
             //id BIGINT
+            //datetime TIMESTAMP
+            //
 
-        dbConstraints: ['NOT NULL','UNIQUE'],//NOT NULL, UNIQUE, PRIMARY KEY, FOREIGN KEY, CHECK, DEFAULT
+        dbConstraints: ['AUTO_INCREMENT','PRIMARY KEY'],//NOT NULL, UNIQUE, PRIMARY KEY, FOREIGN KEY, CHECK, DEFAULT
         validation: {
             string:'',//number,object,string ......i.e all joi base params
             min:3,
@@ -31,5 +35,91 @@ let __defaults={
         }
 
     },
+    
+    {
+        columnName: 'created', 
+        formDataType:'datetime',
+        dbDataType: 'TIMESTAMP',
+        dbConstraints: ['DEFAULT CURRENT_TIMESTAMP'],
+        validation: {            
+        }
+    },    
 
-}
+    {
+        columnName: 'modified', 
+        formDataType:'datetime',
+        dbDataType: 'TIMESTAMP',
+        dbConstraints: ['DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'],
+        validation: {            
+        }
+    },
+
+    {
+        columnName: 'createdBy', 
+        formDataType:'lookup',
+        dbDataType: 'auto',
+        foreignKey:{
+            refrences: "Users",
+            refrencedColumn: "id"
+        },
+        //dbConstraints: [`FOREIGN KEY (createdBy) REFERENCES ${systemTables["Users"]}(id)`],
+        validation:{            
+        }
+    },
+
+    {
+        columnName: 'modifiedBy',
+        formDataType:'lookup',
+        dbDataType: 'auto',
+        foreignKey:{
+            refrences: "Users",
+            refrencedColumn: "id"
+        },
+        dbConstraints: [''],
+        validation: {            
+        }
+    },
+
+    {
+        columnName: 'inheritRowPermissions',
+        formDataType:'bool',
+        dbDataType: 'auto',
+        dbConstraints: [''],
+        validation: {            
+        }
+    },
+
+    {
+        columnName: 'inheritCellPermissions',
+        formDataType:'bool',
+        dbDataType: 'auto',
+        dbConstraints: [''],
+        validation: {            
+        }
+    },
+
+    {//this column is not created for permissions table.
+        columnName: 'rowPerm',
+        formDataType:'lookup',
+        dbDataType: 'auto',
+        foreignKey:{
+            refrences: "Permissions",
+            refrencedColumn: "id",            
+        },
+        dbConstraints: [''],
+        validation: {            
+        }
+    },
+
+    {
+        columnName: 'cellPerm',
+        formDataType:'cluster',
+        dbDataType: 'auto',
+        dbConstraints: [''],
+        validation: {            
+        }
+    },
+
+     
+]
+
---
+++ db/staticSchemas/schemas/__form2dbDatatypeMapping.js
@@ -0,0 +1,9 @@
+let mapping={
+    text :      'VARCHAR(0-65536)',
+    textBox :   'VARCHAR(0-65533)',
+    number:     'VARCHAR(0-255)',
+    cluster:    'JSON',
+    lookup:     'BIGINT',
+    id:         'BIGINT',
+    datetime:   'TIMESTAMP'
+}
